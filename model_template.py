28b4056baf82eb789b2626dde812c21d1d4df4f7
Isak Nordgren <isaknordgren@hotmail.se>
2024-06-20 14:41:46 +0200

    evaluation skeleton created

---
+++ model_template.py
@@ -0,0 +1,11 @@
+
+class Model():
+
+    def __init__(self, modelID):
+        self.modelID = modelID
+
+    def setupModel(self):
+        pass
+
+    def getModelAnswer(self, prompt):
+        pass
