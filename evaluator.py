eb9ddc6fdb17b2dbbd08c9064994b449e3f85f1a
Isak Nordgren <isaknordgren@hotmail.se>
2024-06-20 14:42:30 +0200

    minor

---
+++ evaluator.py
@@ -0,0 +1,21 @@
+
+class Evaluator():
+
+    # Evaluates a given model on the following documents:
+    #   1. Hyresavi 2024
+    #   2. Om krisen eller kriget kommer
+    #   3. 
+
+    def __init__(self, model, loss_fn, metrics):
+        self.model = model
+        
+
+    def evaluate(self):
+        pass
+
+    def evaluate_hyresavi(self):
+        pass
+        # Här vill vi skicka ett antal flervalsfrågor till modellen och be den svara på dem 
+
+    def evaluate_krisen_kriget(self):
+        pass
